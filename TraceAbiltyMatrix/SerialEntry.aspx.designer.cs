8fdc18f8e5e7098a743c23b052812cc8c5c1bf06
SOHELDESAI <sohelldesai@gmail.com>
2025-04-21 12:21:23 +0530

    Code to save data in table and generating ASMID

--- TraceAbiltyMatrix/SerialEntry.aspx.designer.cs
+++ TraceAbiltyMatrix/SerialEntry.aspx.designer.cs
@@ -42,6 +42,24 @@ namespace TraceAbiltyMatrix
         protected global::System.Web.UI.WebControls.Button btnSearch;
 
         /// <summary>
+        /// lblFGSerial control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFGSerial;
+
+        /// <summary>
+        /// txtFGSerial control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFGSerial;
+
+        /// <summary>
         /// gvBOM control.
         /// </summary>
         /// <remarks>
